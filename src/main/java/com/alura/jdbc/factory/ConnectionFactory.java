5e148119ffc1840b041e904f2491ece133414fd4
Matias <matute_11@hotmail.com.ar>
2023-02-12 00:34:10 -0300

    Creando un pool de conexiones

--- src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
+++ src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
@@ -4,13 +4,25 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 public class ConnectionFactory {
 	
+	private DataSource datasource;
+	
+	public ConnectionFactory(){
+		var pooledDataSource = new ComboPooledDataSource();
+		pooledDataSource.setJdbcUrl("jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC");
+		pooledDataSource.setUser("root");
+		pooledDataSource.setPassword("Matiascapo2012.");
+		
+		this.datasource = pooledDataSource;
+	}
+	
 	public Connection recuperaConexion() throws SQLException{
-		return DriverManager.getConnection(
-				"jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC", 
-				"root", 
-				"Matiascapo2012.");
+		return this.datasource.getConnection();
 	}
 	
 }
